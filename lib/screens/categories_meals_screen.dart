0b76a03c945fe71b10c4def19947822814cdecbf
Gabrielhlc <gabrielhenlc@gmail.com>
2023-05-22 13:18:04 -0300

    add: categories meals screen

---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  final Category category;
+
+  const CategoriesMealsScreen(this.category, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Receitas'),
+      ),
+      body: Center(
+        child: Text('Receitas por Categoria ${category.id}'),
+      ),
+    );
+  }
+}
