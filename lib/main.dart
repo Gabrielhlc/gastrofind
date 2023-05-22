5a900f050107d316649172ba6a4f9af735fbbc19
Gabrielhlc <gabrielhenlc@gmail.com>
2023-05-22 13:42:01 -0300

    add: app_routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:gastrofind/screens/categories_screen.dart';
+
+import './screens/categories_meals_screen.dart';
+import './screens/categories_screen.dart';
+
+import './utils/app_routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,16 +13,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
-              .copyWith(secondary: Colors.amber),
-          fontFamily: 'Raleway',
-          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
-          textTheme: ThemeData.light().textTheme.copyWith(
-                  titleLarge: const TextStyle(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
+        fontFamily: 'Raleway',
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: const TextStyle(
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
-              ))),
-      home: const CategoriesScreen(),
+              ),
+            ),
+      ),
+      routes: {
+        AppRoutes.HOME: (ctx) => const CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => const CategoriesMealsScreen(),
+      },
     );
   }
 }
