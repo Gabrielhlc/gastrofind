9d37535374a300c6ee68055fe6179004749abd3b
Gabrielhlc <gabrielhenlc@gmail.com>
2023-05-23 10:22:47 -0300

    add: tabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gastrofind/screens/tabs_screen.dart';
 
 import './screens/categories_meals_screen.dart';
 import './screens/categories_screen.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => const CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => const TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => const CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => const MealDetailScreen(),
       },
